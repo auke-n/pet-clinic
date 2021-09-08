4bcf3669a536b22dd1ef1807d9ed7dad510a5a10
auke-n <brs.morozov@gmail.com>
2021-09-08 15:55:39 +0300

    add iam

---
+++ iac/iam.tf
@@ -0,0 +1,56 @@
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = aws_iam_role.ssm-role.name
+}
+
+
+resource "aws_iam_role" "ssm-role" {
+  name               = "ec2-role"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "ec2-ssm-policy"
+  description = "ec2-ssm-policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "ssm:StartSession",
+                "ssm:TerminateSession",
+                "ssm:ResumeSession",
+                "ssm:DescribeSessions",
+                "ssm:GetConnectionStatus",
+                "s3:*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  policy_arn = aws_iam_policy.policy.arn
+  role = aws_iam_role.ssm-role.name
+}
