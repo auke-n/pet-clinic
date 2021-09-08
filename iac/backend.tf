4850b81365513778ae158fcafa4025fc308a9b53
auke-n <brs.morozov@gmail.com>
2021-09-08 15:55:06 +0300

    modify backend

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "petclinic-tfstate"
-    region = "eu-central-1"
-    key = "terraform.tfstate"
+    bucket  = "petclinic-tfstate"
+    region  = "eu-central-1"
+    key     = "terraform.tfstate"
   }
 }
