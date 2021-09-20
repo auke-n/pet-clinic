f6191a9b88b5814206326e1a27d5b0b91f933517
auke-n <brs.morozov@gmail.com>
2021-09-20 09:26:17 +0300

    add route53

---
+++ iac/route53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_record" "jenkins-rt53" {
+  name = "jenkins.iplatinum.pro"
+  type = "A"
+  zone_id = "Z051803612ODKK7ELB6J8"
+
+  alias {
+    evaluate_target_health = true
+    name = aws_lb.jenkins-lb.dns_name
+    zone_id = aws_lb.jenkins-lb.zone_id
+  }
+}
+
+resource "aws_route53_record" "prod-rt53" {
+  name = "petclinic.iplatinum.pro"
+  type = "A"
+  zone_id = "Z051803612ODKK7ELB6J8"
+
+  alias {
+    evaluate_target_health = true
+    name = aws_lb.prod-lb.dns_name
+    zone_id = aws_lb.prod-lb.zone_id
+  }
+}
