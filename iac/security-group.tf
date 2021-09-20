3d899962b1eab043835465781c5111a64665740e
auke-n <brs.morozov@gmail.com>
2021-09-20 09:24:12 +0300

    modify security-group

--- iac/security-group.tf
+++ iac/security-group.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "jenkins-server-sg" {
-  name = "jenkins-server-sg"
+  name   = "jenkins-server-sg"
   vpc_id = aws_vpc.petclinic.id
 
   ingress {
-    from_port = 8080
-    protocol = "tcp"
-    to_port = 8080
+    from_port       = 8080
+    protocol        = "tcp"
+    to_port         = 8080
     security_groups = [aws_security_group.jenkins-elb-sg.id]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -21,18 +21,18 @@ resource "aws_security_group" "jenkins-server-sg" {
 }
 
 resource "aws_security_group" "jenkins-elb-sg" {
-  name = "jenkins-elb-sg"
+  name   = "jenkins-elb-sg"
   vpc_id = aws_vpc.petclinic.id
   ingress {
-    from_port = 443
-    protocol = "tcp"
-    to_port = 443
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -42,24 +42,24 @@ resource "aws_security_group" "jenkins-elb-sg" {
 }
 
 resource "aws_security_group" "web-server-sg" {
-  name = "web-server-sg"
+  name   = "web-server-sg"
   vpc_id = aws_vpc.petclinic.id
   ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
+    from_port       = 80
+    protocol        = "tcp"
+    to_port         = 80
     security_groups = [aws_security_group.web-elb-sg.id]
   }
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port       = 22
+    protocol        = "tcp"
+    to_port         = 22
     security_groups = [aws_security_group.jenkins-server-sg.id]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -69,18 +69,18 @@ resource "aws_security_group" "web-server-sg" {
 }
 
 resource "aws_security_group" "web-elb-sg" {
-  name = "web-elb-sg"
+  name   = "web-elb-sg"
   vpc_id = aws_vpc.petclinic.id
   ingress {
-    from_port = 443
-    protocol = "tcp"
-    to_port = 443
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -90,18 +90,18 @@ resource "aws_security_group" "web-elb-sg" {
 }
 
 resource "aws_security_group" "build-server-sg" {
-  name = "build-server-sg"
+  name   = "build-server-sg"
   vpc_id = aws_vpc.petclinic.id
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port       = 22
+    protocol        = "tcp"
+    to_port         = 22
     security_groups = [aws_security_group.jenkins-server-sg.id]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
