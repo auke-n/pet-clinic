23588bddf38b3d8b8827b237b8e68bb3b278b055
auke-n <brs.morozov@gmail.com>
2021-09-21 15:14:25 +0300

    modify acm

--- iac/acm.tf
+++ iac/acm.tf
@@ -1,6 +1,6 @@
 // ---------- jenkins -----------------------
 resource "aws_acm_certificate" "jenkins" {
-  domain_name = "jenkins.iplatinum.pro"
+  domain_name       = "jenkins.iplatinum.pro"
   validation_method = "DNS"
 
   tags = {
@@ -12,12 +12,12 @@ resource "aws_acm_certificate_validation" "jenkins-certificate" {
   timeouts {
     create = "5m"
   }
-  certificate_arn = aws_acm_certificate.jenkins.arn
+  certificate_arn         = aws_acm_certificate.jenkins.arn
   validation_record_fqdns = [for record in aws_route53_record.acm-validation1 : record.fqdn]
 }
 
 resource "aws_route53_record" "acm-validation1" {
-for_each = {
+  for_each = {
     for dvo in aws_acm_certificate.jenkins.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
@@ -36,7 +36,7 @@ for_each = {
 // ---------- prod -----------------------
 
 resource "aws_acm_certificate" "petclinic" {
-  domain_name = "petclinic.iplatinum.pro"
+  domain_name       = "petclinic.iplatinum.pro"
   validation_method = "DNS"
 
   tags = {
@@ -48,12 +48,12 @@ resource "aws_acm_certificate_validation" "petclinic-ceritficate" {
   timeouts {
     create = "5m"
   }
-  certificate_arn = aws_acm_certificate.petclinic.arn
+  certificate_arn         = aws_acm_certificate.petclinic.arn
   validation_record_fqdns = [for record in aws_route53_record.acm-validation2 : record.fqdn]
 }
 
 resource "aws_route53_record" "acm-validation2" {
- for_each = {
+  for_each = {
     for dvo in aws_acm_certificate.petclinic.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
