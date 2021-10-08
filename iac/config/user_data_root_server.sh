bb8ac37947f77f97ee1a8718803d1727b1267b9d
auke-n <brs.morozov@gmail.com>
2021-10-08 18:42:32 +0300

    commented SWAP section; moved PS1 top; moved ansible roles and hosts to /etc/ansible/

--- iac/config/user_data_root_server.sh
+++ iac/config/user_data_root_server.sh
@@ -1,19 +1,28 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 echo 'alias ulog="tail -f /var/log/user-data.log"' >> /etc/bashrc
+# PS1
+echo '' >> /etc/bashrc
+echo '# Command prompt customization - $PS1' >> /etc/bashrc
+echo 'if [ "`id -u`" -eq 0 ]; then' >> /etc/bashrc
+echo '    PS1="\[$(tput bold)\]\[\033[38;5;9m\]\u\[$(tput sgr0)\]\[\033[38;5;11m\]@\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;10m\]manager\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]:\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;6m\]\w\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]\\$\[$(tput sgr0)\] "' >> /etc/bashrc
+echo 'else' >> /etc/bashrc
+echo '    PS1="\u\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]@\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;10m\]manager\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]:\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;6m\]\w\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]\\$\[$(tput sgr0)\] "' >> /etc/bashrc
+echo 'fi' >> /etc/bashrc
 
+#=============================================
 amazon-linux-extras install epel -y
 yum update -y
 yum install git -y
 
 
 #Create a SWAP-file
-dd if=/dev/zero of=/swapfile count=2048 bs=1MiB
-chmod 600 /swapfile
-mkswap /swapfile
-swapon  /swapfile
-echo "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
-mount -a
+# dd if=/dev/zero of=/swapfile count=2048 bs=1MiB
+# chmod 600 /swapfile
+# mkswap /swapfile
+# swapon  /swapfile
+# echo "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
+# mount -a
 
 # Add hosts records
 echo "${build_ip} build.server" >> /etc/hosts
@@ -26,6 +35,8 @@ amazon-linux-extras install ansible2
 cd /home/ec2-user/
 git clone https://github.com/auke-n/pet-clinic.git
 cd ./pet-clinic/iac/config/ansible
+cp -r .ansible/roles/* /etc/ansible/roles/
+cp -f hosts /etc/ansible/
 ansible-playbook -i hosts site.yml
 
 # aws s3 cp s3://petclinic01/config/ansible/hosts /etc/ansible/
@@ -45,14 +56,7 @@ ansible-playbook -i hosts site.yml
 ##########################################
 #Server command prompt customization     #
 ##########################################
-# PS1
-echo '' >> /etc/bashrc
-echo '# Command prompt customization - $PS1' >> /etc/bashrc
-echo 'if [ "`id -u`" -eq 0 ]; then' >> /etc/bashrc
-echo '    PS1="\[$(tput bold)\]\[\033[38;5;9m\]\u\[$(tput sgr0)\]\[\033[38;5;11m\]@\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;10m\]manager\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]:\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;6m\]\w\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]\\$\[$(tput sgr0)\] "' >> /etc/bashrc
-echo 'else' >> /etc/bashrc
-echo '    PS1="\u\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]@\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;10m\]manager\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]:\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;6m\]\w\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;11m\]\\$\[$(tput sgr0)\] "' >> /etc/bashrc
-echo 'fi' >> /etc/bashrc
+
 # Handy aliases
 
 echo 'alias ..="cd .."' >> /etc/bashrc
